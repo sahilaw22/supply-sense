52ffa7d174e5f575f0c7aafee143a3db7a39d104
sahilaw22 <sahilaw22@users.noreply.github.com>
2026-07-20 01:15:01 +0530

    fix: set VERCEL_ASGI=1 for FastAPI ASGI support on Vercel Python runtime

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ import sys
 import os
 
 sys.path.insert(0, os.getcwd())
+os.environ['VERCEL_ASGI'] = '1'
 os.environ.setdefault('DATABASE_PATH', os.path.join(os.getcwd(), 'supplysense.db'))
 
 from backend.main import app
