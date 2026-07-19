50ceae7e854ac25636aab085daabfb039147701f
sahilaw22 <sahilaw22@users.noreply.github.com>
2026-07-20 01:28:11 +0530

    fix: replace api/index.py with [[...slug]].py catch-all, add DB auto-generation, remove rewrites

--- api/index.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-sys.path.insert(0, os.getcwd())
-os.environ['VERCEL_ASGI'] = '1'
-os.environ.setdefault('DATABASE_PATH', os.path.join(os.getcwd(), 'supplysense.db'))
-
-from backend.main import app
-
